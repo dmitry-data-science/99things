1894a1d798321d955936a959ad68cb5ac9b6770f
dmitry-data-science <63095652+dmitry-data-science@users.noreply.github.com>
2023-02-15 21:47:47 +0200

    add credentials

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from partners.crest import update_crest
+from credentials import *
 
 items_dict = update_crest()
 print(items_dict)
+
+api_key, shared, secret, shop = get_credentials()
+print(api_key, shared, secret, shop)
