87f0300ed4ce7c4b958781006a718e446171b9b0
dmitry-data-science <63095652+dmitry-data-science@users.noreply.github.com>
2023-02-16 23:57:03 +0200

    implement price update for CREST shop. Test #2

--- partners/crest.py
+++ partners/crest.py
@@ -9,6 +9,6 @@ def update_crest():
 
     variants_dict = get_variants_dict(partners_items_dict, our_shop_product_list)
 
-    # update_variants_quantity(variants_dict)
+    update_variants_quantity(variants_dict)
 
-    return variants_dict
+    return True
